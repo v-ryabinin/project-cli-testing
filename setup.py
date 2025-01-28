1062ac1bf22b857c5c23b6cf9dccd751451755e1
Ryabinin <lozuro777@gmail.com>
2025-01-28 17:27:03 +0500

    add structure first project

---
+++ setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup, find_packages
+setup(name = 'cli_test_project', version='1.0',packages=find_packages())
---
+++ tests/test_foo.py
@@ -0,0 +1,4 @@
+import pytest
+
+def test_1():
+    assert 2 + 2 == 4
