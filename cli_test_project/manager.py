de942d3b29260945b606eeddfebe3d5f3b2e5ce8
Ryabinin <lozuro777@gmail.com>
2025-01-28 19:08:42 +0500

    add count_files and modify manager.py

--- cli_test_project/manager.py
+++ cli_test_project/manager.py
@@ -1,12 +1,21 @@
 import argparse
 
 from cli_test_project.copy import copy_file
+from cli_test_project.count_files import count_files
+from cli_test_project.delete import delete_file
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--command', choices= ['copy'], help='my_command')
+    parser.add_argument('--command', choices= ['copy', 'delete','count'], help='my_command')
+    parser.add_argument('--path', type = str, required=True, help='path to file')
 
     args = parser.parse_args()
     if   args.command == 'copy':
         copy_file('','')
 
+    if args.command == 'delete':
+        delete_file(args.path)
+
+    if args.command == 'count':
+        count=count_files(args.path)
+        print(count)
